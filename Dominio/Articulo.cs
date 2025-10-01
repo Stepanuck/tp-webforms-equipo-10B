8804e6d484e6a49019e2f72548b93c7cc18afa36
Stepanuck <marinozzitomas@hotmail.com>
2025-10-01 19:45:34 -0300

    T1- Agregado Dominio
    Agregado del proyecto dominio con las clases artciulo, categoria, marca

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; } = new Marca();
+        public Categoria Categoria { get; set; } = new Categoria();
+        public decimal Precio { get; set; }
+        
+        public List<Imagen> Imagenes { get; set; } = new List<Imagen>();
+
+
+
+        public Articulo()
+        {
+            Marca = new Marca();
+            Categoria = new Categoria();
+        }
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+}
---
+++ Dominio/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Cliente
+    {
+        public int Id { get; set; }
+        public int Documento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public int CodigoPostal { get; set; } 
+        public Cliente(int id, string nombre, string apellido, string email, string direccion, string ciudad, int codigoPostal)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Direccion = direccion;
+            Ciudad = ciudad;
+            CodigoPostal = codigoPostal;
+        }
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido} - {Email}";
+        }
+    }
+}
