8c544f21875f1d648c85e55f3e2b9d84640d6302
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-10-05 20:43:54 -0300

    CF - 11- AccesoDatos + DatosVaucher + DatosArticulo

---
+++ WebFormsEquipo10B/AccesoDatos/ArticuloDatos.cs
@@ -0,0 +1,60 @@
+﻿using dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace AccesoDatos
+{
+    public class ArticuloDatos
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatosManager manager = new AccesoDatosManager();
+
+            try
+            {
+
+                string consulta = "SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion AS Marca, C.Descripcion AS Categoria, A.Precio, A.IdMarca, A.IdCategoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id";
+                manager.setearConsulta(consulta);
+                manager.ejecutarLectura();
+
+                while (manager.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)manager.Lector["Id"];
+                    aux.Codigo = (string)manager.Lector["Codigo"];
+                    aux.Nombre = (string)manager.Lector["Nombre"];
+                    aux.Descripcion = (string)manager.Lector["Descripcion"];
+
+
+                    if (!(manager.Lector["Precio"] is DBNull))
+                        aux.Precio = (decimal)manager.Lector["Precio"];
+
+
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)manager.Lector["IdMarca"];
+                    aux.Marca.Descripcion = (string)manager.Lector["Marca"];
+
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)manager.Lector["IdCategoria"];
+                    aux.Categoria.Descripcion = (string)manager.Lector["Categoria"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                manager.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ WebFormsEquipo10B/AccesoDatos/VoucherDatos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using dominio.Models;
+
+namespace AccesoDatos
+{
+    public class VoucherDatos
+    {
+        public Voucher buscarPorCodigo(string codigo)
+        {
+            AccesoDatosManager manager = new AccesoDatosManager();
+            try
+            {
+
+                string consulta = "SELECT CodigoVoucher, IdCliente, FechaCanje, IdArticulo FROM VOUCHERS WHERE CodigoVoucher = @codigo";
+                manager.setearConsulta(consulta);
+                manager.setearParametro("@codigo", codigo);
+                manager.ejecutarLectura();
+
+                if (manager.Lector.Read())
+                {
+                    Voucher voucher = new Voucher();
+                    voucher.CodigoVoucher = (string)manager.Lector["CodigoVoucher"];
+
+
+                    if (!(manager.Lector["IdCliente"] is DBNull))
+                        voucher.IdCliente = (int)manager.Lector["IdCliente"];
+
+                    if (!(manager.Lector["FechaCanje"] is DBNull))
+                        voucher.FechaCanje = (DateTime)manager.Lector["FechaCanje"];
+
+                    if (!(manager.Lector["IdArticulo"] is DBNull))
+                        voucher.IdArticulo = (int)manager.Lector["IdArticulo"];
+
+                    return voucher;
+                }
+
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                manager.cerrarConexion();
+            }
+        }
+    }
+}
