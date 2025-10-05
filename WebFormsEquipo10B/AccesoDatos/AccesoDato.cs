07e9443dfc16923784a045f847e86da8769fd915
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-10-04 21:30:39 -0300

    FC - 7 - Conexion a datos

---
+++ WebFormsEquipo10B/AccesoDatos/AccesoDato.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AccesoDatos
+{
+    public class AccesoDatosManager
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatosManager()
+        {
+            // CONEXION FRANCISCO - ¡Actualizada!
+            conexion = new SqlConnection("server=.; database=CATALOGO_P3_DB; integrated security=true;");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        // Dentro de la clase AccesoDatosManager
+
+        public bool ProbarConexion()
+        {
+            try
+            {
+                conexion.Open();
+                conexion.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
--- WebFormsEquipo10B/presentacion/MiMaster.Master.cs
+++ WebFormsEquipo10B/presentacion/MiMaster.Master.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AccesoDatos;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,21 @@ namespace presentacion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // 2. Agregamos la prueba de conexión aquí
+            //    !IsPostBack asegura que la prueba solo se ejecute la primera vez que la página carga.
+            if (!IsPostBack)
+            {
+                AccesoDatosManager manager = new AccesoDatosManager();
+                if (manager.ProbarConexion())
+                {
+                    // Escribe en la ventana "Salida" de Visual Studio
+                    Debug.WriteLine("¡Conexión a la base de datos exitosa desde la Master Page! ✅");
+                }
+                else
+                {
+                    Debug.WriteLine("Falló la conexión a la base de datos desde la Master Page. ❌");
+                }
 
-        }
+            }
     }
 }
