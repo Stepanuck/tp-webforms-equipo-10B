fb7a68047fd82ab1c240a0331a6e31a5471619d0
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-10-05 21:22:08 -0300

    FC - 15 - imagenes

--- WebFormsEquipo10B/AccesoDatos/ArticuloDatos.cs
+++ WebFormsEquipo10B/AccesoDatos/ArticuloDatos.cs
@@ -1,10 +1,7 @@
 ﻿using dominio.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+
 namespace AccesoDatos
 {
     public class ArticuloDatos
@@ -16,7 +13,6 @@ namespace AccesoDatos
 
             try
             {
-
                 string consulta = "SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, M.Descripcion AS Marca, C.Descripcion AS Categoria, A.Precio, A.IdMarca, A.IdCategoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id";
                 manager.setearConsulta(consulta);
                 manager.ejecutarLectura();
@@ -29,10 +25,8 @@ namespace AccesoDatos
                     aux.Nombre = (string)manager.Lector["Nombre"];
                     aux.Descripcion = (string)manager.Lector["Descripcion"];
 
-
                     if (!(manager.Lector["Precio"] is DBNull))
                         aux.Precio = (decimal)manager.Lector["Precio"];
-
 
                     aux.Marca = new Marca();
                     aux.Marca.Id = (int)manager.Lector["IdMarca"];
@@ -45,7 +39,46 @@ namespace AccesoDatos
                     lista.Add(aux);
                 }
 
+                foreach (Articulo articulo in lista)
+                {
+                    articulo.Imagenes = cargarImagenes(articulo.Id);
+                }
+
                 return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                manager.cerrarConexion();
+            }
+        }
+
+        
+        public List<Imagen> cargarImagenes(int idArticulo)
+        {
+            List<Imagen> listaImagenes = new List<Imagen>();
+            AccesoDatosManager manager = new AccesoDatosManager();
+            try
+            {
+                string consulta = "SELECT Id, IdArticulo, ImagenUrl FROM IMAGENES WHERE IdArticulo = @idArticulo";
+                manager.setearConsulta(consulta);
+                manager.setearParametro("@idArticulo", idArticulo);
+                manager.ejecutarLectura();
+
+                while (manager.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.Id = (int)manager.Lector["Id"];
+                    aux.IdArticulo = (int)manager.Lector["IdArticulo"];
+                    
+                    aux.NombreArchivo = (string)manager.Lector["ImagenUrl"];
+                    listaImagenes.Add(aux);
+                }
+
+                return listaImagenes;
             }
             catch (Exception ex)
             {
