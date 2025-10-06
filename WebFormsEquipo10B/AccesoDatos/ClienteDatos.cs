aeb6a7a4959b4e7b05f184bb1e6ca3df1c690d7e
CARRILLO FRANCISCO LUIS <carrillo.francisco.luis@gmail.com>
2025-10-05 21:02:28 -0300

    FC - 13 - Intertar nuevo cliente

--- WebFormsEquipo10B/AccesoDatos/ClienteDatos.cs
+++ WebFormsEquipo10B/AccesoDatos/ClienteDatos.cs
@@ -41,6 +41,36 @@ namespace AccesoDatos
             {
                 manager.cerrarConexion();
             }
+
+        }
+
+        public void insertarNuevo(Cliente nuevo)
+        {
+            AccesoDatosManager manager = new AccesoDatosManager();
+            try
+            {
+                string consulta = "INSERT INTO Clientes (Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP) VALUES (@doc, @nombre, @apellido, @email, @dir, @ciudad, @cp)";
+                manager.setearConsulta(consulta);
+
+                
+                manager.setearParametro("@doc", nuevo.Documento);
+                manager.setearParametro("@nombre", nuevo.Nombre);
+                manager.setearParametro("@apellido", nuevo.Apellido);
+                manager.setearParametro("@email", nuevo.Email);
+                manager.setearParametro("@dir", nuevo.Direccion);
+                manager.setearParametro("@ciudad", nuevo.Ciudad);
+                manager.setearParametro("@cp", nuevo.CP);
+
+                manager.ejecutarAccion(); 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                manager.cerrarConexion();
+            }
         }
     }
 }
